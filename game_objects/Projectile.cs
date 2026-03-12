15eb8377b66928dbc0c2d344de434ffb15f5deb9
Ruben Droogh <rubendroogh@hotmail.nl>
2026-03-12 10:46:19 +0100

    Bouncy ball.

---
+++ game_objects/Projectile.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class Projectile : RigidBody2D
+{
+    [Export]
+    private Vector2 StartingVelocity = new Vector2(500, 0);
+
+    public override void _Ready()
+    {
+        GD.Print("starting velocity: " + StartingVelocity);
+        Start();
+    }
+
+    private void Start()
+    {
+        LinearVelocity = StartingVelocity;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var collisionInfo = MoveAndCollide(LinearVelocity * (float)delta);
+        if (collisionInfo != null)
+        {
+            LinearVelocity = LinearVelocity.Bounce(collisionInfo.GetNormal());
+        }
+    }
+}
