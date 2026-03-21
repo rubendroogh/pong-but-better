8717e84f6c406cb58ce8a5ee39c01e2fe6c33a43
Ruben Droogh <rubendroogh@hotmail.nl>
2026-03-21 11:25:30 +0100

    WIP Damage numbers.

--- screens/battle/ui/DamageNumbers.cs
+++ screens/battle/ui/DamageNumbers.cs
@@ -2,5 +2,7 @@ using Godot;
 
 public partial class DamageNumbers : Node
 {
-    
+    // Listen to signals
+    // Spawn lil damage numbers around
+    // Export: radius, size, 
 }
---
+++ screens/battle/ui/TransientDamageNumber.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class TransientDamageNumber : Node
+{
+    private int LifeTime { get; set; }
+
+    
+}
